a8bd80d81ec1974f1062877d6c2c4bf9d4030229
Nikolay Lapshin <nlapshin1989@gmail.com>
2022-01-01 18:51:23 +0300

    Started server

---
+++ server.js
@@ -0,0 +1,13 @@
+const fastify = require('fastify')
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
